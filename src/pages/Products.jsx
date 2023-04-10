d31c11c3428d8a855699464ab2ffa9750019c4c6
Guapura89 <betocrack1228@gmail.com>
2023-04-09 21:56:18 -0600

    feat: add go back button into products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,8 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 import { ProductCard } from "../components/ProductCard";
 import { useGetProducts } from "../hooks/useGetProducts";
-import { Fade, Zoom } from 'react-awesome-reveal';
+import { Fade } from 'react-awesome-reveal';
+import { Link } from 'react-router-dom';
 
 const API = 'https://api.escuelajs.co/api/v1/products?limit=12&offset=1';
 
@@ -14,6 +15,9 @@ const Products = () => {
 
     return (
         <div className="bg-info">
+            <div>
+                <Link to={'/'}><Button variant="outline-dark" className="m-3 fw-bold">Back</Button></Link>
+            </div>
             <div className="text-center py-3">
                 <h1><Fade cascade duration={200}>Products</Fade></h1>
             </div>
